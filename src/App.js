079e2e51f4da3f50c1b1c20649227d7fb3891bc2
giancarloz <gzabaleta48@gmail.com>
2020-04-15 17:52:07 -0400

    first commit

--- src/App.js
+++ src/App.js
@@ -1,26 +1,64 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Grid from '@material-ui/core/Grid';
 
-function App() {
+
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(2),
+    textAlign: 'center',
+    color: theme.palette.text.secondary,
+  },
+}));
+
+const App = () => {
+  const classes = useStyles();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className={classes.root}>
+      <Grid container spacing={3}>
+        <Grid item xs={12}>
+          <Paper className={classes.paper}>116th Congress</Paper>
+        </Grid>
+        <Grid item xs={12} sm={4}>
+          <Paper className={classes.paper}>Senate</Paper>
+        </Grid>
+        <Grid item xs={12} sm={4}>
+          <Paper className={classes.paper}>Bills</Paper>
+        </Grid>
+        <Grid item xs={12} sm={4}>
+          <Paper className={classes.paper}>House</Paper>
+        </Grid>
+        <Grid item xs={6} sm={4}>
+          <Paper className={classes.paper}>
+            <SenateMembers/>
+          </Paper>
+        </Grid>
+        <Grid item xs={6} sm={4}>
+          <Paper className={classes.paper}>
+            <Bills/>
+          </Paper>
+        </Grid>
+        <Grid item xs={6} sm={4}>
+          <Paper className={classes.paper}>
+            <HouseMembers/>
+          </Paper>
+        </Grid>
+      
+      </Grid>
     </div>
-  );
+  
+  )
+
 }
 
 export default App;
+
+
+
+
+
